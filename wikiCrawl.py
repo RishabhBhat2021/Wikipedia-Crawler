3b5ddedf52b817b9bdf11420eb1a9629d88b76d1
Rishabh Bhat <rishabhbhat20@gmail.com>
2021-10-29 19:32:47 +0530

    First Commit: Added the files

---
+++ wikiCrawl.py
@@ -0,0 +1,43 @@
+from bs4 import BeautifulSoup
+import requests
+
+import re
+
+home_page = "https://en.wikipedia.org/"
+
+search = str(input("Enter anything to search: "))
+
+while search != "Philosophy":
+    search_page = home_page + "wiki/" + search
+
+    request = requests.get(search_page)
+    content = request.text
+    soup = BeautifulSoup(content, 'lxml')
+
+    first_paragraph = soup.find('p', class_=None)
+
+    # Find all paragraphs (i.e. 'p' tags)
+    for paras in soup.find_all('p', class_=None):
+        try:
+            # Check wheather the paragraph is first or not
+            if paras.b.get_text() == search.capitalize(): 
+                # print(paras.b.get_text())
+                first_paragraph = paras
+        except:
+            pass
+
+    # Remove the hyperlinks which are in brackets
+    first_paragraph_s = str(first_paragraph)
+    string=re.sub("\(.*?\)","()",first_paragraph_s) 
+    cut_para = BeautifulSoup(string, 'lxml')
+
+    for tag in cut_para.find_all('a'):
+        if tag.text[0] == "[":
+            tag.decompose()
+
+    # print(cut_para.find('a'))
+
+    if cut_para.find('a') != None:
+        search = cut_para.find('a').attrs['title']
+
+    print(search)
